19c02dc52cdb02163c9042ab080eddac5a481d72
curtyraissa <raissacurty@gmail.com>
2023-03-13 23:28:41 -0300

    importado cssglobal e criado rotas no App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage"
+import { CadastroPage } from "./pages/CadastroPage"
+import { HabitosPage } from "./pages/HabitosPage"
+import { HojePage } from "./pages/HojePage"
+import { HistoricoPage } from "./pages/HistoricoPage"
+
 function App() {
   return (
-    <>
-    App
-    </>
+    <BrowserRouter>
+      App
+
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/cadastro" element={<CadastroPage />} />
+        <Route path="/habitos" element={<HabitosPage />} />
+        <Route path="/hoje" element={<HojePage />} />
+        <Route path="/historico" element={<HistoricoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import GlobalStyle from './styles/GlobalStyle';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );

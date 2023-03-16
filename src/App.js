20dea2f844edb14cce13441474936c9361665fac
curtyraissa <raissacurty@gmail.com>
2023-03-15 23:39:49 -0300

    criado context provide user para compartilhar o state

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import { CadastroPage } from "./pages/CadastroPage/CadastroPage"
 import { HabitosPage } from "./pages/HabitosPage/HabitosPage"
 import { HojePage } from "./pages/HojePage/HojePage"
 import { HistoricoPage } from "./pages/HistoricoPage/HistoricoPage"
-
-// import { Context } from "./contexts/Context";
+import { UserProvider } from "./contexts/context";
 
 function App() {
   return (
-    // <Context.Provider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/cadastro" element={<CadastroPage />} />
-        <Route path="/habitos" element={<HabitosPage />} />
-        <Route path="/hoje" element={<HojePage />} />
-        <Route path="/historico" element={<HistoricoPage />} />
-      </Routes>
-    </BrowserRouter>
-    // </Context.Provider>
+    <UserProvider>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/cadastro" element={<CadastroPage />} />
+          <Route path="/habitos" element={<HabitosPage />} />
+          <Route path="/hoje" element={<HojePage />} />
+          <Route path="/historico" element={<HistoricoPage />} />
+        </Routes>
+      </BrowserRouter>
+
+    </UserProvider>
   );
 }
 
--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -1,3 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const Context = createContext()
+const UserContext = createContext()
+export const UserProvider = () => {
+  // compartilhar statos entre componentes
+  const [user, setUser] = useState({})
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
